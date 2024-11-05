815c0f05037f87b2bc2073fa2e1a441c5251c5ed
Matheus Barbosa <122236890+mthszr@users.noreply.github.com>
2024-11-05 16:29:27 -0300

    refactoring and draw the ball function

--- pong.asm
+++ pong.asm
@@ -4,8 +4,9 @@ STACK ENDS
 
 DATA SEGMENT PARA 'DATA'
 
-    BALL_X DW 0Ah
-    BALL_Y DW 0Ah
+    BALL_X DW 0Ah ;define the x coordinate of the ball
+    BALL_Y DW 0Ah ;define the y coordinate of the ball
+    BALL_SIZE DW 04h ;define the size of the ball (pixels)
 
 DATA ENDS
 
@@ -30,16 +31,39 @@ CODE SEGMENT PARA 'CODE'
         MOV BL,00h  ;specify the background color (black)
         INT 10h     ;execute the configuration
 
-        MOV AH,0Ch  ;function to draw a pixel
-        MOV AL,0Fh  ;choose white as the pixel color (white)
-        MOV BH,00h  ;specify the display page number (page 0)
-        MOV CX,BALL_X ;set the x coordinate
-        MOV DX,BALL_Y ;set the y coordinate
-        INT 10H     ;execute the configuration
-         
+        CALL DRAW_BALL
+
         RET
 
     MAIN ENDP
+
+    DRAW_BALL PROC NEAR
+
+        MOV CX,BALL_X ;set the initial x coordinate
+        MOV DX,BALL_Y ;set the initial y coordinate
+
+        DRAW_BALL_HORIZONTAL:  
+            MOV AH,0Ch  ;function to draw a pixel
+            MOV AL,0Fh  ;choose white as the pixel color (white)
+            MOV BH,00h  ;specify the display page number (page 0)
+            INT 10H     ;execute the configuration
+
+            INC CX      ;move to the next pixel (CX = CX + 1)
+            MOV AX,CX   ;CX - BALL_X > BALL_SIZE ? nex line : next pixel
+            SUB AX,BALL_X
+            CMP AX,BALL_SIZE
+            JNG DRAW_BALL_HORIZONTAL
+
+            MOV CX,BALL_X ;reset the x coordinate
+            INC DX      ;move to the next line (DY = DY + 1)
+
+            MOV AX,DX   ;DX - BALL_Y > BALL_SIZE ? end : next line
+            SUB AX,BALL_Y
+            CMP AX,BALL_SIZE
+            JNG DRAW_BALL_HORIZONTAL
+
+        RET
+    DRAW_BALL ENDP
 
 CODE ENDS
 END MAIN
